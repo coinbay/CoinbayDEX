c6239d4618a612fb5f4983bf036109df6ceecbba
coinbay <37411662+coinbay@users.noreply.github.com>
2018-03-15 18:38:10 +0300

    Update test_etherex.py

--- tests/test_etherex.py
+++ tests/test_etherex.py
@@ -1,6 +1,6 @@
-# etherex.py -- EtherEx tests
+# etherex.py -- CoinbayDEX tests
 #
-# Copyright (c) 2014 EtherEx
+# Copyright (c) 2018 CoinbayDEX
 #
 # This software may be modified and distributed under the terms
 # of the MIT license.  See the LICENSE file for details.
